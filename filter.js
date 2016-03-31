8353eac9f2b85c0d223c35ce6381b37257dafb30
kitakitabauer <kitakita.d.kitakita@gmail.com>
2016-03-31 19:30:53 +0900

    modify filter benchmark

--- filter.js
+++ filter.js
@@ -8,6 +8,17 @@ const items = _.range(100);
 
 suite.run([
   {
+    description: 'Native#filter',
+    exec() {
+      const ret = [];
+      for (let i = 0; i < items.length; i++) {
+        if (items[i] % 2) {
+          ret.push(items[i]);
+        }
+      }
+    }
+  },
+  {
     description: 'Array#filter',
     exec() {
       items.filter(item => item % 2);
